a59406eb9519398628387e58ccd5258d293abb3d
vishnu7075 <106947263+vishnu7075@users.noreply.github.com>
2023-06-26 19:09:11 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,7 +25,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["test/specs/*.js"],
+  specs: ["./test/specs/**/*.js"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
